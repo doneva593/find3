6b59c96b4b378fd0ec1e41126da2a92df000196a
doneva593 <32305276+doneva593@users.noreply.github.com>
2018-07-02 16:42:06 +0100

    Update analysis.go

--- server/main/src/api/analysis.go
+++ server/main/src/api/analysis.go
@@ -9,10 +9,10 @@ import (
 
 	"github.com/pkg/errors"
 	cache "github.com/robfig/go-cache"
-	"github.com/schollz/find3/server/main/src/database"
-	"github.com/schollz/find3/server/main/src/learning/nb1"
-	"github.com/schollz/find3/server/main/src/models"
-	"github.com/schollz/find3/server/main/src/utils"
+	"github.com/doneva593/wifisystem/server/main/src/database"
+	"github.com/doneva593/wifisystem/server/main/src/learning/nb1"
+	"github.com/doneva593/wifisystem/server/main/src/models"
+	"github.com/doneva593/wifisystem/server/main/src/utils"
 )
 
 // AIPort designates the port for the AI processing
