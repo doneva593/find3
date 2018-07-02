c4790eab54edc814777ba41e258eae4ebebedccb
doneva593 <32305276+doneva593@users.noreply.github.com>
2018-07-02 16:45:16 +0100

    Update fingerprints.go

--- server/main/src/api/fingerprints.go
+++ server/main/src/api/fingerprints.go
@@ -4,8 +4,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/schollz/find3/server/main/src/database"
-	"github.com/schollz/find3/server/main/src/models"
+	"github.com/doneva593/wifisystem/server/main/src/database"
+	"github.com/doneva593/wifisystem/server/main/src/models"
 )
 
 type UpdateCounterMap struct {
